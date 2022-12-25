796dc64a62cc5bea4a3d5aafd83ef0faf7f7ae92
Ike <85350135+ikeawesom@users.noreply.github.com>
2022-12-25 12:30:48 +0800

    debug

--- scripts/maintenance.js
+++ scripts/maintenance.js
@@ -1,7 +1,8 @@
 // Maintenance
 localStorage.setItem("maintenance",1);
+alert((localStorage.getItem("maintenance"));
 
 if (localStorage.getItem("maintenance") === "1" && !curPage.includes("/admin/")) {
     alert("here");
-    window.location.href = "../maintenance"
+    window.location.href = "/maintenance"
 }
